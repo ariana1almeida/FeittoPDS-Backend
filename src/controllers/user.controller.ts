ccd53396b7c82b01dc5e625cb7da3c6d53696869
ariana1almeida <arianaleu@hotmail.com>
2025-09-09 19:59:47 -0300

    feat - Criação da lógica de login

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,15 +2,17 @@ import { Request, Response } from "express";
 import { prisma } from "../config/database";
 import { UserRepository } from "../repositories/user.repository";
 import { UserService } from "../services/user.service";
+import axios from "axios";
 
 const userService = new UserService(new UserRepository(prisma));
+const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY as string;
 
 export const createUser = async (req: Request, res: Response) => {
   try {
     const { firstName, lastName, phone, email, password, userType, data } = req.body;
 
     if (!firstName || !lastName || !phone || !email || !password || !userType || !data) {
-      return res.status(400).json({ error: "All fields are required" });
+      return res.status(400).json({ error: "É necessário preencher todos os campos" });
     }
 
     const newUser = await userService.createUser({
@@ -29,6 +31,40 @@ export const createUser = async (req: Request, res: Response) => {
     return res.status(500).json({ error: error.message });
   }
 };
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ error: "É necessário preencher todos os campos"});
+        }
+        console.log("FIREBASE_API_KEY:", FIREBASE_API_KEY);
+        const response = await axios.post(
+            `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`,
+            {
+                email,
+                password,
+                returnSecureToken: true
+            }
+        );
+        type FirebaseLoginResponse = {
+            idToken: string;
+            refreshToken: string;
+            expiresIn: string;
+            localId: string;
+        };
+        const { idToken, refreshToken, expiresIn, localId } = response.data as FirebaseLoginResponse;
+        return res.status(200).json({
+            token: idToken,
+            refreshToken,
+            expiresIn,
+            uid: localId,
+        });
+        }catch (error: any) {
+        console.error(error.response?.data || error.message);
+        return res.status(401).json({ error: "Email ou senha inválidos" });
+    }
+}
 
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
