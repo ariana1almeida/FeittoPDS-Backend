ccdcb22aeedef264b017e51a7855bca877421927
ariana1almeida <arianaleu@hotmail.com>
2025-11-30 18:39:23 -0300

    feature - commit parcial para implementação da feature de chat

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,14 @@ import authRoutes from "./routes/auth.routes";
 import proposalRoutes from "./routes/proposal.routes";
 import ratingsRoutes from "./routes/ratings.routes";
 import serviceRoutes from "./routes/service.routes";
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
 
 const app = express();
+const server = createServer(app);
 const port = process.env.PORT || 3000;
+
 
 connectDatabase();
 
@@ -34,6 +39,15 @@ app.get("/health", (req: Request, res: Response) => {
   });
 });
 
-app.listen(port, () => {
+const socketIo = new Server(server, {
+    cors: { origin: ["http://localhost:5173"], credentials: true },
+});
+
+server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+socketIo.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
